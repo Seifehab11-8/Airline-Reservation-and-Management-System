ad7a9ba7ba1dfa574306d753f41de663ea442223
Seifehab11-8 <semaziz2004@yahoo.com>
2025-04-17 22:45:12 +0200

    testing json nlohmann

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <nlohmann/json.hpp>
+#include <iostream>
+using json = nlohmann::json;
+int main() {
+    json j = {
+        {"name", "Seifalla Ehab Mohamed"},
+        {"ID", 2100619},
+        {"email", "semaziz2003@gmail.com"}
+    };
+    std::cout<<j.dump(4);
+}
