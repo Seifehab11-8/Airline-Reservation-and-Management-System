c8bf78d186e82235750baf56a9c29e148ca4c3a2
Seifehab11-8 <semaziz2003@gmail.com>
2025-04-18 15:32:56 +0200

    testing nholmann/json with its capabilities

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
-#include <../include/nlohmann/json.hpp>
+#include "../include/nlohmann/json.hpp"
 #include <iostream>
+#include <fstream>
+
 using json = nlohmann::json;
 int main() {
-    json j = {
-        {"name", "Seifalla Ehab Mohamed"},
-        {"ID", 2100619},
-        {"email", "semaziz2003@gmail.com"}
-    };
-    std::cout<<j.dump(4);
+    std::ifstream inputFile("C:\\Users\\semaz\\OneDrive\\Documents\\Software_Development\\Airline-Reservation-and-Management-System\\storage\\test1.json");
+    json j = json::parse(inputFile).at(1);
+    json jn = {{"name", "ahmed"}};
+    // j.push_back(jn);
+    std::cout<<j["name"]<<std::endl;
+    std::cout<<j["id"]<<std::endl;
+    // for(auto n: j) {
+    //     std::cout<<n.dump(4);
+    //     // std::cout<<std::endl;
+    // }
+    std::ofstream outputFile("../storage/out.json");
+    outputFile << jn;
 }
