4a2848406dd77c2357f2fddc43d3f3fee3a6a36e
Seifehab11-8 <semaziz2004@yahoo.com>
2025-04-20 17:47:34 +0200

    more reseach into how to store files

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,52 @@
 #include <fstream>
 
 using json = nlohmann::json;
+
+void appendJsonObject(const std::string& filename, const json& newObject) {
+    std::fstream file(filename, std::ios::in | std::ios::out);
+    
+    if (!file.is_open()) {
+        throw std::runtime_error("Cannot open file");
+    }
+
+    // Find the last bracket
+    file.seekg(-1, std::ios::end);
+    char lastChar;
+    file.get(lastChar);
+    std::cout<<lastChar;
+    
+    if (lastChar != ']') {
+        throw std::runtime_error("Invalid JSON array format");
+    }
+
+    // Move cursor back to overwrite the closing bracket
+    
+    // Add comma if not first element (check if file has more than just [])
+    file.seekg(0, std::ios::end);
+    // we know the size of the file from tellg how?
+    //!since tellg specify the position of pointer as offset from the bgeinning
+    // say you have a file of 3 bytes going to last position make tellg return 2 as its offset from ios_begin
+    if (file.tellg() > 2) {
+        file.seekp(-1, std::ios::end);
+        file << ",";
+    }
+    
+    // Write the new object and close the array
+    file << newObject.dump() << "]";
+    file.close();
+}
+
 int main() {
-    std::ifstream inputFile("C:\\Users\\semaz\\OneDrive\\Documents\\Software_Development\\Airline-Reservation-and-Management-System\\storage\\test1.json");
-    json j = json::parse(inputFile).at(1);
-    json jn = {{"name", "ahmed"}};
-    // j.push_back(jn);
-    std::cout<<j["name"]<<std::endl;
-    std::cout<<j["id"]<<std::endl;
-    // for(auto n: j) {
-    //     std::cout<<n.dump(4);
-    //     // std::cout<<std::endl;
-    // }
-    std::ofstream outputFile("../storage/out.json");
-    outputFile << jn;
+    json newObject = {
+        {"name", "ahmed"},
+        {"id", 2100000}
+    };
+
+    try {
+        appendJsonObject("../storage/test1.json", newObject);
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
 }
